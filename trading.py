51ddafc79153721a507c3f660a3aae0fd007b0e2
VectorLineal <jnobzaa@unal.edu.co>
2020-02-11 19:24:37 -0500

    se añadee el algoritmo de trading de quantipian

---
+++ trading.py
@@ -0,0 +1,236 @@
+import quantopian.algorithm as algo
+import quantopian.optimize as opt
+from quantopian.pipeline import Pipeline
+from quantopian.pipeline.factors import SimpleMovingAverage
+
+from quantopian.pipeline.filters import QTradableStocksUS
+from quantopian.pipeline.experimental import risk_loading_pipeline
+
+from quantopian.pipeline.data.psychsignal import stocktwits
+from quantopian.pipeline.data import Fundamentals
+
+# Constraint Parameters
+MAX_GROSS_LEVERAGE = 2.0
+TOTAL_POSITIONS = 10
+MIN_NET_EXPOSURE = -0.012
+MAX_NET_EXPOSURE = 0.012
+MIN_NET_GROUP_EXPOSURE = -0.02
+MAX_NET_GROUP_EXPOSURE = 0.02
+
+# Here we define the maximum position size that can be held for any
+# given stock. If you have a different idea of what these maximum
+# sizes should be, feel free to change them. Keep in mind that the
+# optimizer needs some leeway in order to operate. Namely, if your
+# maximum is too small, the optimizer may be overly-constrained.
+
+MAX_SHORT_POSITION_SIZE = 0.5 / TOTAL_POSITIONS
+MAX_LONG_POSITION_SIZE = 0.5 / TOTAL_POSITIONS
+
+
+def initialize(context):
+    context.msft = sid(5061)
+    context.amzn = sid(16841)
+    context.sne = sid(6984)
+    
+    algo.attach_pipeline(make_pipeline(), 'long_short_equity_template')
+
+    # Attach the pipeline for the risk model factors that we
+    # want to neutralize in the optimization step. The 'risk_factors' string is 
+    # used to retrieve the output of the pipeline in before_trading_start below.
+    algo.attach_pipeline(risk_loading_pipeline(), 'risk_factors')
+
+    # Schedule our rebalance function
+    algo.schedule_function(func=rebalance,
+                           date_rule=algo.date_rules.week_start(days_offset=2),
+                           time_rule=algo.time_rules.market_open(hours=0, minutes=30),
+                           half_days=True)
+
+    # Record our portfolio variables at the end of day
+    algo.schedule_function(func=record_vars,
+                           date_rule=algo.date_rules.every_day(),
+                           time_rule=algo.time_rules.market_close(),
+                           half_days=True)
+
+
+def make_pipeline():
+    value = Fundamentals.ebit.latest / Fundamentals.enterprise_value.latest
+    quality = Fundamentals.roe.latest
+    sentiment_score = SimpleMovingAverage(
+        inputs=[stocktwits.bull_minus_bear],
+        window_length=3,
+    )
+
+    universe = QTradableStocksUS()
+    
+    # We winsorize our factor values in order to lessen the impact of outliers
+    # For more information on winsorization, please see
+    # https://en.wikipedia.org/wiki/Winsorizing
+    value_winsorized = value.winsorize(min_percentile=0.05, max_percentile=0.95)
+    quality_winsorized = quality.winsorize(min_percentile=0.05, max_percentile=0.95)
+    sentiment_score_winsorized = sentiment_score.winsorize(min_percentile=0.05,                                                                             max_percentile=0.95)
+
+    # Here we combine our winsorized factors, z-scoring them to equalize their influence
+    combined_factor = (
+        value_winsorized.zscore() + 
+        quality_winsorized.zscore() + 
+        sentiment_score_winsorized.zscore()
+    )
+
+    # Build Filters representing the top and bottom baskets of stocks by our
+    # combined ranking system. We'll use these as our tradeable universe each
+    # day.
+    longs = combined_factor.top(TOTAL_POSITIONS//2, mask=universe)
+    shorts = combined_factor.bottom(TOTAL_POSITIONS//2, mask=universe)
+
+    # The final output of our pipeline should only include
+    # the top/bottom 300 stocks by our criteria
+    long_short_screen = (longs | shorts)
+    working_capital_per_share = Fundamentals.working_capital_per_share.latest
+    trailing_dividend_yield = Fundamentals.trailing_dividend_yield.latest
+    forward_dividend_yield = Fundamentals.forward_dividend_yield.latest
+    growth_score = Fundamentals.growth_score.latest
+    peg_ratio = Fundamentals.peg_ratio.latest
+    total_revenue = Fundamentals.total_revenue.latest
+    
+
+    # Create pipeline
+    pipe = Pipeline(
+        columns={
+            'longs': longs,
+            'shorts': shorts,
+            'combined_factor': combined_factor,
+            'working capital per share': working_capital_per_share,
+            'trailing_dividend_yield': trailing_dividend_yield,
+            'forward_dividend_yield': forward_dividend_yield,
+            'growth_score': growth_score,
+            'total_revenue': total_revenue,
+            'peg_ratio':peg_ratio
+        },
+        screen=long_short_screen
+    )
+    return pipe
+
+
+def before_trading_start(context, data):
+    """
+    Optional core function called automatically before the open of each market day.
+
+    Parameters
+    ----------
+    context : AlgorithmContext
+        See description above.
+    data : BarData
+        An object that provides methods to get price and volume data, check
+        whether a security exists, and check the last time a security traded.
+    """
+    # Call algo.pipeline_output to get the output
+    # Note: this is a dataframe where the index is the SIDs for all
+    # securities to pass my screen and the columns are the factors
+    # added to the pipeline object above
+    context.pipeline_data = algo.pipeline_output('long_short_equity_template')
+
+    # This dataframe will contain all of our risk loadings
+    context.risk_loadings = algo.pipeline_output('risk_factors')
+
+
+def record_vars(context, data):
+    """
+    A function scheduled to run every day at market close in order to record
+    strategy information.
+
+    Parameters
+    ----------
+    context : AlgorithmContext
+        See description above.
+    data : BarData
+        See description above.
+    """
+    # Plot the number of positions over time.
+    algo.record(num_positions=len(context.portfolio.positions))
+
+
+# Called at the start of every month in order to rebalance
+# the longs and shorts lists
+def rebalance(context, data):
+    """
+    A function scheduled to run once every Monday at 10AM ET in order to
+    rebalance the longs and shorts lists.
+
+    Parameters
+    ----------
+    context : AlgorithmContext
+        See description above.
+    data : BarData
+        See description above.
+    """
+    # Retrieve pipeline output
+    pipeline_data = context.pipeline_data
+
+    risk_loadings = context.risk_loadings
+
+    # Here we define our objective for the Optimize API. We have
+    # selected MaximizeAlpha because we believe our combined factor
+    # ranking to be proportional to expected returns. This routine
+    # will optimize the expected return of our algorithm, going
+    # long on the highest expected return and short on the lowest.
+    objective = opt.MaximizeAlpha(pipeline_data.combined_factor)
+
+    # Define the list of constraints
+    constraints = []
+    # Constrain our maximum gross leverage
+    constraints.append(opt.MaxGrossExposure(MAX_GROSS_LEVERAGE))
+
+    # Require our algorithm to remain dollar neutral
+    constraints.append(opt.DollarNeutral())
+
+    # Add the RiskModelExposure constraint to make use of the
+    # default risk model constraints
+    neutralize_risk_factors = opt.experimental.RiskModelExposure(
+        risk_model_loadings=risk_loadings,
+        version=0
+    )
+    constraints.append(neutralize_risk_factors)
+
+    # With this constraint we enforce that no position can make up
+    # greater than MAX_SHORT_POSITION_SIZE on the short side and
+    # no greater than MAX_LONG_POSITION_SIZE on the long side. This
+    # ensures that we do not overly concentrate our portfolio in
+    # one security or a small subset of securities.
+    constraints.append(
+        opt.NetExposure(
+            min=MIN_NET_EXPOSURE,
+            max=MAX_NET_EXPOSURE
+        ))
+    
+    labels = {context.msft: 'TECH', context.amzn: 'TECH', context.sne: 'TECH'}
+    
+    constraints.append(
+        opt.NetGroupExposure.with_equal_bounds(
+            labels = labels,
+            min=MIN_NET_GROUP_EXPOSURE,
+            max=MAX_NET_GROUP_EXPOSURE
+        ))
+    
+    constraints.append(
+        opt.PositionConcentration.with_equal_bounds(
+            min=-MAX_SHORT_POSITION_SIZE,
+            max=MAX_LONG_POSITION_SIZE
+        ))
+    
+    
+    constrain_sector_style_risk = opt.experimental.RiskModelExposure(  
+        context.risk_loadings,  
+        version=opt.Newest,  
+
+    )  
+    constraints.append(constrain_sector_style_risk)
+
+    # Put together all the pieces we defined above by passing
+    # them into the algo.order_optimal_portfolio function. This handles
+    # all of our ordering logic, assigning appropriate weights
+    # to the securities in our universe to maximize our alpha with
+    # respect to the given constraints.
+    algo.order_optimal_portfolio(
+        objective=objective,
+        constraints=constraints
+    )
